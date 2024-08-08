45e6ce8cad5c52a0db11cb87fe5cca0461de362e
wendyzhaogogo <zhaowenzzl@163.com>
2024-08-08 23:01:05 +0800

    fix: it

--- apps/my-app/src/pages/AutoVoice/generate.ts
+++ apps/my-app/src/pages/AutoVoice/generate.ts
@@ -1,13 +1,15 @@
 async function downloadFromLink(word: string) {
-  const el_a = document.createElement('a');
-  el_a.download = word;
-  el_a.target = '_blank';
-  const url = `https://fanyi.baidu.com/gettts?lan=zh&spd=5&text=${encodeURIComponent(word)}`;
-  console.log(url);
-  el_a.href = url;
-  document.body.appendChild(el_a);
-  el_a.click();
-  el_a.remove();
+  //   setTimeout(() => {
+  // const el_a = document.createElement('a');
+  // el_a.download = word;
+  // el_a.target = '_blank';
+  const url = `https://fanyi.baidu.com/gettts?lan=zh&text=${encodeURIComponent(word)}&spd=5`;
+  //     el_a.href = url;
+  //     document.body.appendChild(el_a);
+  //     el_a.click();
+  //     el_a.remove();
+  //   }, 500);
+  window.open(url);
   // const url = `https://fanyi.baidu.com/gettts?lan=zh&spd=5&text=${encodeURIComponent(word)}`;
   // const response = await fetch(url);
   // const blob = await response.blob();
